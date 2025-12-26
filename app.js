d73ff4673b71164f11690761d8d43a10098b444f
ccastrom <claudiocastrom7@gmail.com>
2025-12-25 23:58:13 -0300

    fix: correct 'app.js' function reference name

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { createServer } from 'node:http';
-import {getLiveData} from './riotLiveClient.js';
+import {getLiveGameData} from './riotLiveClient.js';
 const server = createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World!\n');
@@ -8,4 +8,4 @@ server.listen(3000, '127.0.0.1', () => {
   console.log('Listening on 127.0.0.1:3000');
 });
 
-getLiveData();
+getLiveGameData();
